0718630830709c0f8301d4bac0883cde205321a4
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-09-15 17:32:37 -0700

    Create double_pointers.cpp

---
+++ double_pointers.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+	Pointers point to other variables or objects in the program. This means that the value 
+	that the pointer stores is an address. However, pointers can't just point to any variable 
+	of any data type since it is limited to one data type. Pointers are declared with the * 
+	symbol as well as a data type. The data type denotes that the pointer is able to store 
+	addresses that hold values of that data type.
+	
+	Ex.
+	int *x;
+	
+	The above example shows that x is a pointer of type int. This means that it can hold 
+	the address of int variables.
+	
+	Ex.
+	int **x;
+	
+	The above example shows that x is a pointer of type int pointer. This means that it 
+	can hold the address of int pointer variables.
+	
+	Setting the value of a pointer to some address is known as referencing.
+	
+	Ex.
+	int *p = &x;
+	
+	Retrieving the value stored in a memory address is known as dereferencing.
+	
+	Ex.
+	
+	cout << *p;
+*/
+int main()
+{
+	
+	
+	return 0;
+}
