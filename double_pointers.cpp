6bc943f8227ec59f5661426011845770e6887233
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-09-16 19:54:12 -0700

    Update double_pointers.cpp

--- double_pointers.cpp
+++ double_pointers.cpp
@@ -35,7 +35,36 @@ using namespace std;
 */
 int main()
 {
+	// creating and setting int variable
+	int i;
+	i = 5;
 	
+	// creating and setting pointer variable of type int
+	int *ip;
+	ip = &i; // referencing or setting ip to i's memory address
+	
+	// creating and setting pointer variable of type int pointer
+	int **ipp;
+	ipp = &ip; // referencing or setting ipp to ip's memory address
+	
+	cout << "Printing each variable value:" << endl;
+	
+	// each of these will print the variable's stored value
+	cout << i << endl; // prints the number 5
+	cout << ip << endl; // prints the memory address of i
+	cout << ipp << endl; // prints the memory address of ip
+	cout << endl;
+	
+	cout << "Print the value that is stored in the address that the pointers are set to:" << endl;
+	
+	// these will print the value that is stored in the 
+	// address that the pointers are referencing/set to.
+	// getting the value of a memory address is also known as dereferencing
+	cout << *ip << endl;
+	cout << *ipp << endl;
+	cout << endl;
+	
+	// print memory addresses
 	
 	return 0;
 }
